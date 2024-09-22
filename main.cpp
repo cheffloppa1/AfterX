4ef17cb1bf25e7ab17345e9f5a5b136579540356
cheffloppa1 <chefseller8@gmail.com>
2024-09-22 13:20:12 +0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+	DWORD processID = 12345; //replace here with your process ID
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
+
+	DWORD ammoAddress = 0x12345678; //replace here with your ammo adress 
+	int newAmmo = 10000;
+
+	WriteProcessMemory(hProcess, (LPVOID)ammoAddress, &newAmmo, sizeof(newAmmo), NULL);
+
+	CloseHandle(hProcess);
+	return 0;
+}
